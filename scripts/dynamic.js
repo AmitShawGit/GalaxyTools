9bf8374d3e773c944c96f46a920752b4d40f356c
amit <amitshaw1421@gmail.com>
2025-12-26 11:38:58 +0530

    frromtemd done

--- scripts/dynamic.js
+++ scripts/dynamic.js
@@ -1,4 +1,5 @@
 const trending_blade = document.getElementById('trending_blade');
+const trending_drill = document.getElementById('trending_drill');
 
 const fetchBlades = () => {
     fetch('./scripts/products.json')
@@ -6,8 +7,13 @@ const fetchBlades = () => {
         .then(data => {
 
             let output = "";
-            
-            let limitedProduct = data.slice(0,8)
+            let categories = data.categories
+
+
+            let getGetCategoryName = categories.find(item => item.name == "blade")
+
+
+            let limitedProduct = getGetCategoryName?.children?.slice(0, 8)
 
             limitedProduct?.map((item) => {
                 return (
@@ -41,4 +47,52 @@ const fetchBlades = () => {
 
 }
 
+
+const fetchDrills = () => {
+    fetch('./scripts/products.json')
+        .then(res => res.json())
+        .then(data => {
+
+            let output = "";
+            let categories = data.categories
+
+
+            let getGetCategoryName = categories.find(item => item.name == "drills")
+            console.log(getGetCategoryName);
+
+            let limitedProduct = getGetCategoryName?.children?.slice(0, 8)
+
+            limitedProduct?.map((item) => {
+                return (
+                    output += `
+                      <div class="col-lg-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <span class="trendy">${item?.trendy}</span >
+                                <img src="./img/${item?.img}" alt="blade" class="img-fluid">
+                            </div>
+                            <h3>${item?.name}</h3>
+                            <p>${item?.shortDesc}</p>
+                            <p class="tooth">${item?.toothProfile}</p>
+                            <p class="desc">${item?.desc}</p>
+                            <ul class="ratings">
+                                <li><i class="bi bi-star-fill"></i></li>
+                                <li><i class="bi bi-star-fill"></i></li>
+                                <li><i class="bi bi-star-fill"></i></li>
+                                <li><i class="bi bi-star-fill"></i></li>
+                                <li><i class="bi bi-star-fill"></i></li>
+                                <li>(2 review)</li>
+                            </ul>
+                        </div >
+                    </div > `
+                )
+            })
+
+            trending_drill.innerHTML = output
+
+        })
+}
+
+
 fetchBlades()
+fetchDrills()
