2c4c5e40348b83f036df91d31adc8521554a9c44
amit <amitshaw1421@gmail.com>
2025-12-30 21:27:51 +0530

    category done

--- scripts/dynamic.js
+++ scripts/dynamic.js
@@ -1,5 +1,7 @@
 const trending_blade = document.getElementById('trending_blade');
 const trending_drill = document.getElementById('trending_drill');
+const test = document.getElementById('myTab');
+const myTabContent = document.getElementById('myTabContent');
 
 const fetchBlades = () => {
     fetch('./scripts/products.json')
@@ -94,5 +96,61 @@ const fetchDrills = () => {
 }
 
 
+const fetchBlade = () => {
+    fetch("./scripts/products.json")
+        .then(res => res.json())
+        .then(data => {
+
+            let tabHTML = "";
+            let contentHTML = "";
+
+            data.categories.forEach((category, catIndex) => {
+
+                // Tabs
+                tabHTML += `
+          <li class="nav-item">
+            <button class="nav-link ${catIndex === 0 ? "active" : ""}"
+              data-bs-toggle="tab"
+              data-bs-target="#tab-${catIndex}">
+              ${category.name}
+            </button>
+          </li>
+        `;
+
+                // Tab Content
+                contentHTML += `
+          <div class="tab-pane fade ${catIndex === 0 ? "show active" : ""}"
+            id="tab-${catIndex}">
+            <div class="row">
+        `;
+
+                category.children.forEach(item => {
+                    contentHTML += `
+            <div class="col-lg-3">
+              <div class="card">
+                <div class="card-img">
+                  <span class="trendy">${item.trendy ?? ""}</span>
+                  <img src="./img/${item.img}" class="img-fluid">
+                </div>
+                <h3>${item.name}</h3>
+                <p>${item.shortDesc}</p>
+                <p class="tooth">${item.toothProfile}</p>
+                <p class="desc">${item.desc}</p>
+              </div>
+            </div>
+          `;
+                });
+
+                contentHTML += `</div></div>`;
+            });
+
+            document.getElementById("tabs").innerHTML = tabHTML;
+            document.getElementById("tabContent").innerHTML = contentHTML;
+        });
+};
+
+
 fetchBlades()
 fetchDrills()
+
+fetchBlade()
